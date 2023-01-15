ed0131f58061746e5cd8622165965a2e6f63a602
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-01-16 00:32:04 +0300

    16.01.2023

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,21 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+/* alert('Hello'); */
 
-number = 10;
-console.log(number); 
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); */
+
+const user = "Maxim";
+
+alert(`Привет, ${user}`);
