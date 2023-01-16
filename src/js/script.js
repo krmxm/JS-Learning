6071a787c58d139b1aa7456eef1851dfc385c228
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-01-17 00:45:57 +0300

    first app

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,7 @@ document.write(answers); */
 
 alert(`Привет, ${user}`); */
 
-console.log('arr' + " - object"); //строка
+/* console.log('arr' + " - object"); //строка
 console.log(4 + " - object"); //строка
 console.log(4 + +"5"); //type - number, + перед кавычками (унарный плюс) превращает строку в число
 
@@ -26,10 +26,35 @@ let incr = 10,
     decr = 10;
 
 incr++; // увеличение значения на единицу, постфиксная форма записи
-decr--; // уменьшение значения на единицу, постфиксная форма записи
+decr--; // уменьшение значения на единицу, постфиксная форма записи */
 
 /* ++incr; // префиксная форма записи 
 --decr; // префиксная форма записи */
 
-console.log(incr);
-console.log(decr);
+/* console.log(incr);
+console.log(decr); */
+
+
+//First app
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '1');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+  'count': numberOfFilms,
+  'movies': {},
+  'actors': {},
+  'genres': [],
+  'privat': false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
