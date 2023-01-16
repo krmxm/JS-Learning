28bc54579998298965f57b492af143dfa7547358
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-01-16 21:04:48 +0300

    16.01.test git

--- src/js/script.js
+++ src/js/script.js
@@ -9,13 +9,27 @@ console.log(result); */
 console.log(answer + 5); */
 
 /* const answers = [];
-
 answers[0] = prompt('Как ваше имя?', '');
 answers[1] = prompt('Как ваша фамилия?', '');
 answers[2] = prompt('Сколько вам лет?', '');
-
 document.write(answers); */
 
-const user = "Maxim";
+/* const user = "Maxim";
 
-alert(`Привет, ${user}`);
+alert(`Привет, ${user}`); */
+
+console.log('arr' + " - object"); //строка
+console.log(4 + " - object"); //строка
+console.log(4 + +"5"); //type - number, + перед кавычками (унарный плюс) превращает строку в число
+
+let incr = 10,
+    decr = 10;
+
+incr++; // увеличение значения на единицу, постфиксная форма записи
+decr--; // уменьшение значения на единицу, постфиксная форма записи
+
+/* ++incr; // префиксная форма записи 
+--decr; // префиксная форма записи */
+
+console.log(incr);
+console.log(decr);
