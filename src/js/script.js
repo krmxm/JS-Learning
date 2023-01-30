74bf539a5f66615d264b13f73b62e55a4e9be764
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-01-30 23:23:19 +0300

    peredacha ssilky

--- src/js/script.js
+++ src/js/script.js
@@ -778,3 +778,102 @@ console.log(fib(4));
   const products = str.split(", "); // то что ввёл пользователь переводит в массив
   products.sort(); // сортировка в алфавитном порядке
   console.log(products.join('; ')); // переводит массив в строку */
+
+  // Передача по ссылке или по значению, Spread оператор
+
+/*   let a = 5,
+      b = a;
+
+  b = b + 5;
+
+  console.log(b);
+  console.log(a); */
+
+/*   const obj = {
+    a: 5,
+    b: 1
+  };
+
+  const copy = obj; // ссылка
+
+  copy.a = 10;
+
+  console.log(copy);
+  console.log(obj); */
+
+  function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+      objCopy[key] = mainObj [key];
+    }
+
+    return objCopy;
+  }
+
+  const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+  };
+
+  const newNumbers = copy(numbers);
+
+  newNumbers.a = 10;
+  newNumbers.c.x = 10;
+
+/*   console.log(newNumbers);
+  console.log(numbers); */
+
+  const add = {
+    d: 17,
+    e: 20
+  };
+
+  const clone = Object.assign({}, add);
+
+  clone.d = 20;
+
+/*   console.log(add);
+  console.log(clone); */
+
+  const oldArray = ['a', 'b', 'c'];
+  const newArray = oldArray.slice();
+  
+  newArray[1] = 'afdsfref';
+  console.log(newArray);
+  console.log(oldArray);
+
+  const video = ['youtube', 'vimeo', 'rutube'],
+        blogs = ['wordpress', 'livejournal', 'blogger'],
+        internet = [...video, ...blogs, 'vk', 'facebook'];
+
+  console.log(internet);
+
+  function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+  }
+
+  const num = [2, 5, 7];
+
+  log(...num);
+
+  const array = ["a", "b"];
+
+  const newAaray = [...array];
+
+  const q = {
+    one: 1,
+    two: 2
+  };
+
+  const newObj = {...q};
+
+  console.log(newObj);
+  console.log(q);
